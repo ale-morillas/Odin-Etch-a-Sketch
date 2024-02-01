0cf318f9a7213449e34f595ea1ae51f4bd87e0d8
ale-morillas <alecorm.edx@gmail.com>
2024-02-01 08:41:08 -0500

    Add drawBoard function to create the grid

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
 // Board
 const board = document.querySelector(".container-board");
-let boardSide = parseInt(
-  window.getComputedStyle(board).getPropertyValue("width")
-);
-const gridSize = 52;
-const defaultDimensions = boardSide / gridSize;
+const gridSize = 16;
 
-// Grid
-// Create Row and Column divs
-for (let i = 0; i < gridSize; i++) {
-  // Create the rows
-  const rowDiv = document.createElement("div");
-  rowDiv.className = "rowDiv";
-  board.appendChild(rowDiv);
-  //Create the margins
+// Drawing board
+function drawBoard(gridSize = 16) {
+  // Board properties
+  const boardSide = parseInt(
+    window.getComputedStyle(board).getPropertyValue("width")
+  );
+  const defaultDimensions = boardSide / gridSize;
+  // Grid
+  // Create Row and Column divs
+  for (let i = 0; i < gridSize; i++) {
+    // Create the rows
+    const rowDiv = document.createElement("div");
+    rowDiv.className = "rowDiv";
+    board.appendChild(rowDiv);
+    //Create the margins
 
-  for (let j = 0; j < gridSize; j++) {
-    // Create the columns
-    const columnItem = document.createElement("div");
-    columnItem.className = "columnItem";
-    rowDiv.appendChild(columnItem);
+    for (let j = 0; j < gridSize; j++) {
+      // Create the columns
+      const columnItem = document.createElement("div");
+      columnItem.className = "columnItem";
+      rowDiv.appendChild(columnItem);
 
-    //Columns dimensions
-    columnItem.style.height = defaultDimensions + "px";
-    columnItem.style.width = defaultDimensions + "px";
-    // Columns styles
-    columnItem.style.backgroundColor = "white";
-    columnItem.style.border = "solid 1px #2e1760";
+      //Columns dimensions
+      columnItem.style.height = defaultDimensions + "px";
+      columnItem.style.width = defaultDimensions + "px";
+      // Columns styles
+      columnItem.style.backgroundColor = "white";
+      columnItem.style.border = "solid 1px #2e1760";
+    }
+    rowDiv.style.height = defaultDimensions + "px";
   }
-  rowDiv.style.height = defaultDimensions + "px";
 }
+
+drawBoard(100);
